5298be3593b70b75cb82115e4c44505dc91799c9
ArturR <a.rzeszutko6@gmail.com>
2022-04-03 20:56:17 +0200

    rank grades

---
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+
+namespace GradeBook.GradeBooks
+{
+    public class RankedGradeBook : BaseGradeBook
+    {
+        public RankedGradeBook(string name) : base(name)
+        {
+            Type = Enums.GradeBookType.Ranked;
+        }
+
+        public override char GetLetterGrade(double averageGrade)
+        {
+            if (Students.Count < 5) throw new InvalidOperationException();
+
+            int betterStudentsCount = 0;
+            for (int i = 0; i < Students.Count; i++)
+            {
+                if (Students[i].AverageGrade > averageGrade) betterStudentsCount++;
+            }
+            double rank = (double)(betterStudentsCount + 1) / Students.Count;
+            Console.WriteLine(rank);
+            if (rank <= .2) return 'A';
+            else if (rank <= .4) return 'B';
+            else if (rank <= .6) return 'C';
+            else if (rank <= .8) return 'D';
+            else return 'F';
+        }
+
+        public override void CalculateStatistics()
+        {
+            if (Students.Count < 5) Console.WriteLine("Ranked grading requires at least 5 students.");
+            else base.CalculateStatistics();
+        }
+
+        public override void CalculateStudentStatistics(string name)
+        {
+            if (Students.Count < 5) Console.WriteLine("Ranked grading requires at least 5 students.");
+            else base.CalculateStudentStatistics(name);
+        }
+    }
+}
